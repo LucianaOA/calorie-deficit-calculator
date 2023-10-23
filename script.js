56d31ebcea487a72b1e4873812d9cd36afac57d2
LucianaOA <69084008+LucianaOA@users.noreply.github.com>
2023-10-23 15:04:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,18 +18,27 @@ document.addEventListener('DOMContentLoaded', function () {
         const calorieDeficit = calculateCalorieDeficit(tdee, goalWeight);
 
         // Display the result on the page
-        document.getElementById('calorie-deficit').textContent = calorieDeficit;
+        document.getElementById('calorie-deficit').textContent = `${calorieDeficit} calories`;
     });
 });
 
 function calculateBMR(weight, height, age) {
-    // BMR calculation logic
+    // Calculate BMR using a formula (e.g., Harris-Benedict equation)
+    // Replace the following placeholder formula with the actual calculation
+    const bmr = 10 * weight + 6.25 * height - 5 * age;
+    return bmr;
 }
 
 function calculateTDEE(bmr, activityLevel) {
-    // TDEE calculation logic
+    // Calculate TDEE by multiplying BMR with activity level
+    // Replace the following placeholder formula with the actual calculation
+    const tdee = bmr * activityLevel;
+    return tdee;
 }
 
 function calculateCalorieDeficit(tdee, goalWeight) {
-    // Calorie deficit calculation logic
+    // Calculate the calorie deficit based on the goal weight
+    // Replace the following placeholder formula with the actual calculation
+    const calorieDeficit = tdee - (goalWeight * 500); // Placeholder formula
+    return calorieDeficit;
 }
