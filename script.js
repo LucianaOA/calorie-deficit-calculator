b6b6c65ff0836bed190f8711890a97cbb9cb53c3
LucianaOA <69084008+LucianaOA@users.noreply.github.com>
2023-10-23 14:18:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-// Your JavaScript logic for calculating calorie deficit
-// You can use the script provided earlier for this purpose
+document.addEventListener('DOMContentLoaded', function () {
+    const calculateBtn = document.getElementById('calculate-btn');
+
+    calculateBtn.addEventListener('click', function () {
+        const currentWeight = parseFloat(document.getElementById('current-weight').value);
+        const height = parseFloat(document.getElementById('height').value);
+        const age = parseInt(document.getElementById('age').value);
+        const goalWeight = parseFloat(document.getElementById('goal-weight').value);
+        const activityLevel = parseFloat(document.getElementById('activity-level').value);
+
+        // Calculate BMR (Basal Metabolic Rate)
+        const bmr = calculateBMR(currentWeight, height, age);
+
+        // Calculate TDEE (Total Daily Energy Expenditure)
+        const tdee = calculateTDEE(bmr, activityLevel);
+
+        // Calculate calorie deficit
+        const calorieDeficit = calculateCalorieDeficit(tdee, goalWeight);
+
+        // Display the result on the page
+        document.getElementById('calorie-deficit').textContent = calorieDeficit;
+    });
+});
+
+function calculateBMR(weight, height, age) {
+    // BMR calculation logic
+}
+
+function calculateTDEE(bmr, activityLevel) {
+    // TDEE calculation logic
+}
+
+function calculateCalorieDeficit(tdee, goalWeight) {
+    // Calorie deficit calculation logic
+}
